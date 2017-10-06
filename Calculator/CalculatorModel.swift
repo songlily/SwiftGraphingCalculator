39ac1c411b4677e2bedc8fcc22682190ad2244b7
Lily Song <lilysong@Lilys-MBP.lan>
2017-10-06 19:53:58 -0400

    Updated Assignment 1

--- Calculator/CalculatorModel.swift
+++ Calculator/CalculatorModel.swift
@@ -24,168 +24,159 @@ func fact(n: Double) -> Double {                                            // f
 
 struct CalculatorModel {
     
-    private var accumulator: Double?
-    private var accumDescription = " "                                      // created to log all operators+operands in an equation for description
-    
+    private var accumulated: (value: Double?, description: String) = (nil, " ")
+    private var operand: String?
+    private var lastUnary = false                                                   //checks if last operation was a unary operation
+
     private enum Operation {
         case clear
         case constant(Double)
-        case unaryOperation((Double) -> Double)
-        case binaryOperation((Double, Double) -> Double)
+        case unaryOperation((Double) -> Double, (String) -> String)
+        case binaryOperation((Double, Double) -> Double, (Void) -> String) //(String, String) -> String)
         case equals
     }
     
     private var operations: [String:Operation] = [
         "C": Operation.clear,
 
-        "+": Operation.binaryOperation(+),
-        "−": Operation.binaryOperation(-),
-        "×": Operation.binaryOperation(*),
-        "÷": Operation.binaryOperation(/),
+        "+": Operation.binaryOperation(+, {" + "}),
+        "−": Operation.binaryOperation(-, {" - "}),
+        "×": Operation.binaryOperation(*, {" × "}),
+        "÷": Operation.binaryOperation(/, {" ÷ "}),
         "=": Operation.equals,
         
-        "±": Operation.unaryOperation({-$0}),                       //before
-        "x²": Operation.unaryOperation({$0*$0}),                       //after
-        "√": Operation.unaryOperation(sqrt),                       //before
-        "!": Operation.unaryOperation(fact),                        //after
+        "±": Operation.unaryOperation({-$0}, {"-(\($0))"}),                       //before
+        "x²": Operation.unaryOperation({$0*$0}, {"(\($0))²"}),                       //after
+        "√": Operation.unaryOperation(sqrt, {"√(\($0))"}),                       //before
+        "!": Operation.unaryOperation(fact, {"\($0)!"}),                        //after
         
-        "%": Operation.unaryOperation({$0/100}),                       //after
+        "%": Operation.unaryOperation({$0/100}, {"\($0)"}),                       //after
         "π": Operation.constant(Double.pi),
         "e": Operation.constant(M_E),
-        "ln": Operation.unaryOperation(log),                       //before
+        "ln": Operation.unaryOperation(log, {"ln(\($0))"}),                       //before
 
-        "sin": Operation.unaryOperation(sin),                       //before
-        "cos": Operation.unaryOperation(cos),
-        "tan": Operation.unaryOperation(tan)
-        
+        "sin": Operation.unaryOperation(sin, {"sin(\($0))"}),                       //before
+        "cos": Operation.unaryOperation(cos, {"cos(\($0))"}),
+        "tan": Operation.unaryOperation(tan, {"tan(\($0))"})
     ]
     
     mutating func performOperation (_ symbol: String) {
         
-        accumDescription = accumDescription.replacingOccurrences(of: " ...", with: "")          //remove ellipsis
+        let descriptionHasEquals = accumulated.description.contains("=")
+        accumulated.description = accumulated.description.replacingOccurrences(of: " ...", with: "")          //remove ellipsis
         
         if let operation = operations[symbol]  {
             switch operation {
-            case .constant(let value):
-                if (lastUnary || accumDescription.contains("=")) {                                          //if last operation was a unary operation, clear description
-                    accumDescription = " "
-                }
-                accumulator = value
-                accumDescription += "\(symbol)"                                //show symbol for constants in description
                 
-            case .unaryOperation (let function):
-                if lastUnary {
-                    accumDescription = " "
+            case .constant(let value):
+                if (lastUnary || descriptionHasEquals) {                                          //if last operation was a unary operation, clear description
+                    accumulated.description = " "
                 }
-                let descriptionHasEquals = accumDescription.contains("=")
-                accumDescription = accumDescription.components(separatedBy: " ").dropLast().joined(separator: " ")
-                if accumulator != nil {
-                    if (symbol == "x²" || symbol == "!" || symbol == "%") {                                     //used an if statement to separate the symbols that go after the number
-                        if descriptionHasEquals {
-                        accumDescription = " (\(accumDescription))\(symbol)"
-                        } else {
-                            accumDescription += " \(stringValueForAccumulator())\(symbol)"
-                        }
-                        accumDescription = accumDescription.replacingOccurrences(of: "x", with: "")             //take out x if x²
+                accumulated = (value, addToDescription("\(symbol)"))
+                
+            case .unaryOperation (let function, let descriptionFunction):
+                if lastUnary {
+                    accumulated.description = " "
+                }
+                
+                accumulated.description = accumulated.description.components(separatedBy: " ").dropLast().joined(separator: " ")
+                
+                if accumulated.value != nil {
+                    if descriptionHasEquals {
+                        accumulated = (function(accumulated.value!), descriptionFunction("\(accumulated.description)"))                   //formatting??
                     } else {
-                        if descriptionHasEquals {
-                            accumDescription = " \(symbol) (\(accumDescription))"                          //show " (symbol)"
-                        } else {
-                            accumDescription += " \(symbol) (\(stringValueForAccumulator()))"
-                        }
+                        accumulated = (function(accumulated.value!), addToDescription(descriptionFunction(stringAccumulatedValue())))                   //formatting??
                     }
                 }
-                accumulator = function(accumulator!)
+                
                 lastUnary = true
                 
-            case .binaryOperation (let function):
-                if accumDescription.contains("=") {                                         // if already contains complete equation, clear description
-                    accumDescription = accumDescription.components(separatedBy: " ").dropLast().joined(separator: " ")
+            case .binaryOperation (let function, let descriptionFunction):
+                if descriptionHasEquals {                                         // if already contains complete equation, clear description
+                    accumulated.description = accumulated.description.components(separatedBy: " ").dropLast().joined(separator: " ")
                 }
-                if accumulator != nil {
-                    if (accumDescription == " ") {
-                        accumDescription += stringValueForAccumulator()
-                    }
-                    if resultIsPending {                                                //if pending binary operations, process
-                        accumulator = pendingBinaryOperation!.perform(with: accumulator!)
-                    }
-                    pendingBinaryOperation = PendingBinOp (function: function, firstOp: accumulator!)
+                
+                if accumulated.value != nil {
                     
-                    accumDescription = accumDescription.replacingOccurrences(of: " ...", with: "")
-                    accumDescription += " \(symbol) "                             //show " (symbol) "
-                    accumulator = nil
+                    //first runthrough, save function as function, accumulator as value // accumDescription as savedDescription
+                    //second+ runthough, use saved function+value and return new value, (save function as function, accumulator as value)
+                    
+                    if resultIsPending {
+                        accumulated.value = (pendingBinaryOperation!.perform(with: accumulated.value!))
+                        
+                    }
+                    pendingBinaryOperation = PendingBinOp (function: function, firstOperand: accumulated.value!)
+                    
+                    accumulated = (nil, addToDescription(descriptionFunction()))
                     lastUnary = false
                 }
                 
             case .equals:
-                if accumDescription.contains("=") {                                         // if already contains complete equation, clear description
-                    accumDescription = " "
-                }
-                if accumulator != nil {
-                    if resultIsPending {                                                    //process pending binary operations if any
-                        accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+                if accumulated.value != nil && !descriptionHasEquals {
+                    if resultIsPending {
+                        accumulated.value = pendingBinaryOperation!.perform(with: accumulated.value!)
                         pendingBinaryOperation = nil
                     }
-                    accumDescription += " ="
+                    accumulated.description = addToDescription(" =")
                 }
                 lastUnary = false
                 
             case .clear:                                                        //clears all values or returns to default
-                accumulator = 0
-                accumDescription = " "
+                accumulated = (0, " ")
                 pendingBinaryOperation = nil
                 lastUnary = false
             }
-            
-        }
-        if resultIsPending {                                                    //use the bool to check if I should add ellipsis
-            accumDescription += " ..."
         }
         
+        if resultIsPending {
+            accumulated.description = addToDescription(" ...")
+        }
     }
     
-    private func stringValueForAccumulator() -> String {
-        if Double(Int(accumulator!)) == accumulator! {
-            return String(Int(accumulator!))
+    private func stringAccumulatedValue() -> String {
+        if Double(Int(accumulated.value!)) == accumulated.value! {
+            return String(Int(accumulated.value!))
         } else {
-            return String(accumulator!)
+            return String(accumulated.value!)
+        }
+    }
+    
+    private func addToDescription (_ addition: String) -> String {
+        let newDescription = accumulated.description + addition
+        return newDescription
+    }
+    
+    private var pendingBinaryOperation: PendingBinOp?
+    private struct PendingBinOp {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform (with secondOperand: Double) -> Double {
+            return function (firstOperand, secondOperand)
         }
     }
     
     private var resultIsPending : Bool {
         return pendingBinaryOperation != nil
     }
-    
-    var description: String? {                                                    //lists what user has typed; use resultIsPending Bool??
-        get {
-            return accumDescription                                                 //accumulate elsewhere?
-        }
-        // set
-    }
-
-    private var lastUnary = false                                                   //checks if last operation was a unary operation
-    private var pendingBinaryOperation: PendingBinOp?
-
-    private struct PendingBinOp {
-        let function: (Double, Double) -> Double
-        let firstOp: Double
-        
-        func perform (with secondOp: Double) -> Double {
-            return function (firstOp, secondOp)
-        }
-    }
 
     mutating func setOperand (_ operand: Double) {
-        accumulator = operand
-        if accumDescription.contains("=") || lastUnary {                                         // if already contains complete equation, clear
-            accumDescription = " "
+        accumulated.value = operand
+        if accumulated.description.contains("=") || lastUnary {                                         // if already contains complete equation, clear
+            accumulated.description = " "
         }
-        accumDescription += stringValueForAccumulator()                           //add to description whatever user types in
+        accumulated.description = addToDescription(stringAccumulatedValue())
+    }
+    
+    var description: String? {                                                    //lists what user has typed
+        get {
+            return accumulated.description
+        }
     }
     
     var result: Double? {
         get {
-            return accumulator
+            return accumulated.value
         }
     }
 }
