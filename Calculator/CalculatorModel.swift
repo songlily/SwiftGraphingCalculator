38b887ec4c7b33dcf49f53252322b253138effb2
Lily Song <lilysong@Lilys-MBP.lan>
2017-10-08 13:46:20 -0400

    IP Assignment 2

--- Calculator/CalculatorModel.swift
+++ Calculator/CalculatorModel.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 func fact(n: Double) -> Double {                                            // factorial function
-    if n >= 0 {
+    if (n >= 0) && (Double(Int(n)) == n) {
         var a = n, b = 1.0
         while a != 0 {
             b = b * a
@@ -27,12 +27,16 @@ struct CalculatorModel {
     private var accumulated: (value: Double?, description: String) = (nil, " ")
     private var operand: String?
     private var lastUnary = false                                                   //checks if last operation was a unary operation
+    
+    //**********
+    var externalExpressionArray = [String?] ()             //array components of description/input
+    
 
     private enum Operation {
         case clear
         case constant(Double)
         case unaryOperation((Double) -> Double, (String) -> String)
-        case binaryOperation((Double, Double) -> Double, (Void) -> String) //(String, String) -> String)
+        case binaryOperation((Double, Double) -> Double, (Void) -> String)                              //(String, String) -> String)
         case equals
     }
     
@@ -45,50 +49,57 @@ struct CalculatorModel {
         "÷": Operation.binaryOperation(/, {" ÷ "}),
         "=": Operation.equals,
         
-        "±": Operation.unaryOperation({-$0}, {"-(\($0))"}),                       //before
-        "x²": Operation.unaryOperation({$0*$0}, {"(\($0))²"}),                       //after
-        "√": Operation.unaryOperation(sqrt, {"√(\($0))"}),                       //before
-        "!": Operation.unaryOperation(fact, {"\($0)!"}),                        //after
+        "±": Operation.unaryOperation({-$0}, {" -(\($0))"}),                       //before
+        "x²": Operation.unaryOperation({$0*$0}, {" (\($0))²"}),                       //after
+        "√": Operation.unaryOperation(sqrt, {" √(\($0))"}),                       //before
+        "!": Operation.unaryOperation(fact, {" \($0)!"}),                        //after
         
-        "%": Operation.unaryOperation({$0/100}, {"\($0)"}),                       //after
+        "%": Operation.unaryOperation({$0/100}, {" \($0)%"}),                       //after
         "π": Operation.constant(Double.pi),
         "e": Operation.constant(M_E),
-        "ln": Operation.unaryOperation(log, {"ln(\($0))"}),                       //before
+        "ln": Operation.unaryOperation(log, {" ln(\($0))"}),                       //before
 
-        "sin": Operation.unaryOperation(sin, {"sin(\($0))"}),                       //before
-        "cos": Operation.unaryOperation(cos, {"cos(\($0))"}),
-        "tan": Operation.unaryOperation(tan, {"tan(\($0))"})
+        "sin": Operation.unaryOperation(sin, {" sin(\($0))"}),                       //before
+        "cos": Operation.unaryOperation(cos, {" cos(\($0))"}),
+        "tan": Operation.unaryOperation(tan, {" tan(\($0))"})
     ]
     
     mutating func performOperation (_ symbol: String) {
+        
+        //**********
+        externalExpressionArray.append(symbol)
         
         let descriptionHasEquals = accumulated.description.contains("=")
         accumulated.description = accumulated.description.replacingOccurrences(of: " ...", with: "")          //remove ellipsis
         
         if let operation = operations[symbol]  {
+            
             switch operation {
                 
             case .constant(let value):
                 if (lastUnary || descriptionHasEquals) {                                          //if last operation was a unary operation, clear description
                     accumulated.description = " "
+                    //**********
+                    externalExpressionArray = []
                 }
                 accumulated = (value, addToDescription("\(symbol)"))
+                
                 
             case .unaryOperation (let function, let descriptionFunction):
                 if lastUnary {
                     accumulated.description = " "
+                    //**********
+                    externalExpressionArray = []
                 }
-                
                 accumulated.description = accumulated.description.components(separatedBy: " ").dropLast().joined(separator: " ")
-                
                 if accumulated.value != nil {
                     if descriptionHasEquals {
-                        accumulated = (function(accumulated.value!), descriptionFunction("\(accumulated.description)"))                   //formatting??
+                        accumulated = (function(accumulated.value!), descriptionFunction("\(accumulated.description)"))
                     } else {
-                        accumulated = (function(accumulated.value!), addToDescription(descriptionFunction(stringAccumulatedValue())))                   //formatting??
+                        accumulated = (function(accumulated.value!), addToDescription(descriptionFunction(stringAccumulatedValue())))
+
                     }
                 }
-                
                 lastUnary = true
                 
             case .binaryOperation (let function, let descriptionFunction):
@@ -103,12 +114,12 @@ struct CalculatorModel {
                     
                     if resultIsPending {
                         accumulated.value = (pendingBinaryOperation!.perform(with: accumulated.value!))
-                        
                     }
                     pendingBinaryOperation = PendingBinOp (function: function, firstOperand: accumulated.value!)
                     
                     accumulated = (nil, addToDescription(descriptionFunction()))
                     lastUnary = false
+                    
                 }
                 
             case .equals:
@@ -118,6 +129,7 @@ struct CalculatorModel {
                         pendingBinaryOperation = nil
                     }
                     accumulated.description = addToDescription(" =")
+                    
                 }
                 lastUnary = false
                 
@@ -125,6 +137,7 @@ struct CalculatorModel {
                 accumulated = (0, " ")
                 pendingBinaryOperation = nil
                 lastUnary = false
+                
             }
         }
         
@@ -166,6 +179,9 @@ struct CalculatorModel {
             accumulated.description = " "
         }
         accumulated.description = addToDescription(stringAccumulatedValue())
+        //**********
+        externalExpressionArray.append("\(operand)")
+        
     }
     
     var description: String? {                                                    //lists what user has typed
@@ -179,4 +195,144 @@ struct CalculatorModel {
             return accumulated.value
         }
     }
+    
+    
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    
+    // expression array: ["7", "+", "9", "√"]
+    
+    
+    func evaluate() -> (result: Double?, isPending: Bool, description: String) {
+        //
+        
+        var expressionArray = externalExpressionArray             //array components of description/input
+
+        var result: Double?
+        var description = " "
+        var lastUnary = false
+        var pendingBinaryOperation: PendingBinOp?
+        
+        var isPending : Bool {
+            return pendingBinaryOperation != nil
+        }
+        
+        
+        func stringResult() -> String {
+            if Double(Int(result!)) == result! {
+                return String(Int(result!))
+            } else {
+                return String(result!)
+            }
+        }
+        
+        func setOperand(_ operand: Double) {
+            result = operand
+            description += stringResult()
+        }
+        
+        func performOperation (_ symbol: String) {
+            
+            let descriptionHasEquals = description.contains("=")
+            description = description.replacingOccurrences(of: " ...", with: "")          //remove ellipsis
+            
+            if let operation = operations[symbol]  {
+                switch operation {
+                    
+                case .constant(let value):
+                    if (lastUnary || descriptionHasEquals) {                                          //if last operation was a unary operation, clear description
+                        description = " "
+                    }
+                    result = value
+                    description += "\(symbol)"
+                    
+                case .unaryOperation (let function, let descriptionFunction):
+                    if lastUnary {
+                        description = " "
+                    }
+                    description = description.components(separatedBy: " ").dropLast().joined(separator: " ")
+                    if result != nil {
+                        
+                        if descriptionHasEquals {                                                   //how to append to expressionArray??
+                            description = descriptionFunction(description)
+                        } else {
+                            description += descriptionFunction(stringResult())
+                        }
+                        result = function(result!)
+                    }
+                    lastUnary = true
+                    
+                case .binaryOperation (let function, let descriptionFunction):
+                    if descriptionHasEquals {                                         // if already contains complete equation, clear description
+                        description = description.components(separatedBy: " ").dropLast().joined(separator: " ")
+                    }
+                    
+                    if result != nil {
+                        //first runthrough, save function as function, accumulator as value // accumDescription as savedDescription
+                        //second+ runthough, use saved function+value and return new value, (save function as function, accumulator as value)
+                        
+                        if isPending {
+                            result = (pendingBinaryOperation!.perform(with: result!))
+                        }
+                        pendingBinaryOperation = PendingBinOp (function: function, firstOperand: result!)
+                        
+                        result = nil
+                        description += descriptionFunction()
+                        lastUnary = false
+                    }
+                    
+                case .equals:
+                    if result != nil && !descriptionHasEquals {
+                        if isPending {
+                            result = pendingBinaryOperation!.perform(with: result!)
+                            pendingBinaryOperation = nil
+                        }
+                        description += (" =")
+                    }
+                    lastUnary = false
+                    
+                case .clear:                                                        //clears all values or returns to default
+                    result = 0
+                    description = " "
+                    pendingBinaryOperation = nil
+                    lastUnary = false
+                }
+            }
+            
+            if isPending {
+                description += (" ...")
+            }
+        }
+        
+        for item in expressionArray {
+            if Double(item!) != nil {
+                setOperand(Double(item!)!)
+            } else {
+                performOperation(item!)
+            }
+        }
+        
+        //
+        return (result, isPending, description)
+    }
+    
+    
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    
+    /*mutating func setOperand (variable named: String) {
+        operand = named
+        if accumulated.description.contains("=") || lastUnary {                                         // if already contains complete equation, clear
+            accumulated.description = " "
+        }
+        accumulated.description = addToDescription(operand!)
+    }
+    
+    private enum Expression {
+        case operand (Double)
+        case variable (String)          //((String) -> Double)
+        indirect case operation (Expression, Expression?)
+    }
+     */
+
 }
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,24 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
-    @IBOutlet weak var typed: UILabel!                              //label for description
+    @IBOutlet weak var typed: UILabel!                    //label for description
+    private var model = CalculatorModel ()
     
+    private var saved = ["M":  0.0]
+
     var isTyping = false
-    
-    @IBAction func touchDigit(_ sender: UIButton) {
-        let digit = sender.currentTitle!
-        
-        if isTyping {
-            if (digit != "." || !display.text!.contains(".")) {        //if num or no deci, register
-                let textDisplayed = display.text!
-                display.text = textDisplayed + digit
-                            }
-        } else {
-            display.text = digit
-            isTyping = true
-        }
-    }
-    
     var displayValue: Double {
         get {
             return Double(display.text!)!
@@ -42,10 +30,22 @@ class ViewController: UIViewController {
         }
     }
     
-    private var model = CalculatorModel ()
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if isTyping {
+            if (digit != "." || !display.text!.contains(".")) {        //if num or no deci, register
+                let textDisplayed = display.text!
+                display.text = textDisplayed + digit
+            }
+        } else {
+            display.text = digit
+            isTyping = true
+        }
+    }
     
     @IBAction func performOperation(_ sender: UIButton) {
-
+        
         if isTyping {
             model.setOperand(displayValue)
             isTyping = false
@@ -54,13 +54,34 @@ class ViewController: UIViewController {
         if let mathSymbol = sender.currentTitle {
             model.performOperation(mathSymbol)
         }
-        if let result = model.result{
+        
+        let evaluatedResult = model.evaluate()
+        
+        //if let result = model.result {
+        if let result = evaluatedResult.result {
             displayValue = result
-            
+        
+        }
+        //typed.text = model.description
+        typed.text = evaluatedResult.description                  //description shows up in label
+    }
+    
+    @IBAction func memory(_ sender: UIButton) {
+        
+        /*if sender.currentTitle == "→M" {
+            saved["M"] = displayValue
+            isTyping = false
         }
         
-        typed.text = model.description                      //description shows up in label
-
+        if sender.currentTitle == "M" {
+            model.setOperand(variable: "M")
+            isTyping = false
+        }
+        
+        let evaluatedResult = model.evaluate(using: saved)
+        displayValue = evaluatedResult.result ?? 0       //could be nil
+        typed.text = evaluatedResult.description*/
+        
     }
     
 }
